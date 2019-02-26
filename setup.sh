75f86d24ab76fc7c5035f288abde45625540ca86
sanket-s-patil <47996032+sanket-s-patil@users.noreply.github.com>
2019-02-26 00:36:13 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+sudo apt update
 sudo apt-get install -y mosquitto mosquitto-clients
 sudo apt-get install -y python3 
 sudo apt-get install -y python3-pip
@@ -7,3 +8,6 @@ pip3 install paho-mqtt
 sudo /etc/init.d/mosquitto stop
 sudo cp mosquitto.conf /etc/mosquitto/conf.d
 sudo /etc/init.d/mosquitto start
+sudo apt install -y mongodb
+sudo systemctl restart mongodb
+mongo --eval 'db.runCommand({ connectionStatus: 1 })'
