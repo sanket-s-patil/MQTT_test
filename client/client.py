2844625d2bf87df4d642c1e18ac0277c404faa74
sank1990 <sank1990@nasdrive.hsd1.ca.comcast.net>
2019-02-26 13:58:32 +0530

    counter implemented

--- client/client.py
+++ client/client.py
@@ -1,12 +1,13 @@
 import paho.mqtt.publish as publish
 import time
-
+count=0
 MQTT_SERVER = "13.59.175.31"
 MQTT_PATH = "test_channel"
 while 1:
- print("Sending 0...")
- publish.single(MQTT_PATH, "0", hostname=MQTT_SERVER)
+ count=count+1
+ print("Sending count...")
+ print(count)
+ publish.single(MQTT_PATH, count, hostname=MQTT_SERVER)
  time.sleep(1)
- print("Sending 1...")
- publish.single(MQTT_PATH, "1", hostname=MQTT_SERVER)
+ publish.single(MQTT_PATH, "Counter", hostname=MQTT_SERVER)
  time.sleep(1)
