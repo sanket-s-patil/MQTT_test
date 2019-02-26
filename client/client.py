1232a196811fb8894afe5acad3873ff0f7c13c5e
sank1990 <sank1990@nasdrive.hsd1.ca.comcast.net>
2019-02-26 06:42:36 +0530

    updated

--- client/client.py
+++ client/client.py
@@ -3,9 +3,10 @@ import time
 
 MQTT_SERVER = "13.59.175.31"
 MQTT_PATH = "test_channel"
-
-print("Sending 0...")
-publish.single(MQTT_PATH, "0", hostname=MQTT_SERVER)
-time.sleep(1)
-print("Sending 1...")
-publish.single(MQTT_PATH, "1", hostname=MQTT_SERVER)
+while 1:
+ print("Sending 0...")
+ publish.single(MQTT_PATH, "0", hostname=MQTT_SERVER)
+ time.sleep(1)
+ print("Sending 1...")
+ publish.single(MQTT_PATH, "1", hostname=MQTT_SERVER)
+ time.sleep(1)
