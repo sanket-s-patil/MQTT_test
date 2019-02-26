174f7b5014e6bd3f81fb3a05c104c7eb5848cdca
sanket-s-patil <47996032+sanket-s-patil@users.noreply.github.com>
2019-02-26 00:34:57 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,4 +4,6 @@ sudo apt-get install -y python3
 sudo apt-get install -y python3-pip
 pip3 install --upgrade pip
 pip3 install paho-mqtt
-
+sudo /etc/init.d/mosquitto stop
+sudo cp mosquitto.conf /etc/mosquitto/conf.d
+sudo /etc/init.d/mosquitto start
